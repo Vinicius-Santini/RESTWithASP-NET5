3990cf93abae0d2bcc7ebdb7a19787bb0756be73
Vinicius Santini <vinicius2000.santini@hotmail.com>
2021-02-23 13:01:08 -0300

    migrations implemented and book endpoints created

---
+++ RestWithASPNET/RestWithASPNET/Controllers/BookController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithASPNET.Model;
+using RestWithASPNET.Business;
+
+namespace RestWithASPNET.Controllers
+{
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class BookController : ControllerBase
+    {
+       
+
+        private readonly ILogger<BookController> _logger;
+        private IBookBusiness _booksBusiness;
+        public BookController(ILogger<BookController> logger, IBookBusiness booksBusiness)
+        {
+            _logger = logger;
+            _booksBusiness = booksBusiness;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_booksBusiness.FindAll());
+        }
+        
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var book = _booksBusiness.FindById(id);
+
+            if (book == null) return NotFound();
+
+            return Ok(book);
+        }
+
+        [HttpPost()]
+        public IActionResult Post([FromBody] Book book)
+        {
+
+            if (book == null) return BadRequest();
+
+            return Ok(_booksBusiness.Create(book));
+        }
+        
+        [HttpPut()]
+        public IActionResult Put([FromBody] Book book)
+        {
+
+            if (book == null) return BadRequest();
+
+            return Ok(_booksBusiness.Update(book));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _booksBusiness.Delete(id);
+
+            return NoContent();
+        }
+
+    }
+}
--- RestWithASPNET/RestWithASPNET/Startup.cs
+++ RestWithASPNET/RestWithASPNET/Startup.cs
@@ -17,17 +17,24 @@ using RestWithASPNET.Business;
 using RestWithASPNET.Business.Implementations;
 using RestWithASPNET.Repository;
 using RestWithASPNET.Repository.Implementations;
+using Serilog;
 
 namespace RestWithASPNET
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             Configuration = configuration;
-        }
+            Environment = environment;
 
-        public IConfiguration Configuration { get; }
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -38,17 +45,25 @@ namespace RestWithASPNET
             var connection = Configuration["MySQLConnection:MySQLConnectionString"];
             services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
 
+            if (Environment.IsDevelopment())
+            {
+                MigrateDatabase(connection);
+            }
+
             services.AddApiVersioning();
 
             services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
             services.AddScoped<IPersonRepository, PersonRepositoryImplementation>();
+
+            services.AddScoped<IBookBusiness, BookBusinessImplementation>();
+            services.AddScoped<IBookRepository, BookRepositoryImplementation>();
 
 
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestWithASPNET", Version = "v1" });
             });
-        }
+        } 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -70,6 +85,25 @@ namespace RestWithASPNET
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private void MigrateDatabase(string connection)
+        {
+            try
+            {
+                var evolveConnection = new MySql.Data.MySqlClient.MySqlConnection(connection);
+                var evolve = new Evolve.Evolve(evolveConnection, msg => Log.Information(msg))
+                {
+                    Locations = new List<string> { "db/migrations", "db/dataset" },
+                    IsEraseDisabled = true,
+                };
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Database migration failed", ex);
+                throw;
+            }
         }
     }
 }
