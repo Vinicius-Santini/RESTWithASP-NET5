dca9147330b863bfbec81c6a3935412063000c32
Vinicius Santini <vinicius2000.santini@hotmail.com>
2021-02-26 00:52:45 -0300

    interfaces and consts created

---
+++ RestWithASPNET/RestWithASPNET/Hypermedia/Abstract/IResponseEnricher.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Hypermedia.Abstract
+{
+    public interface IResponseEnricher
+    {
+        bool CanEnrich(ResultExecutedContext context);
+        Task Enrich(ResultExecutedContext context);
+
+    }
+}
---
+++ RestWithASPNET/RestWithASPNET/Hypermedia/Abstract/ISupportHyperMedia.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RestWithASPNET.Hypermedia.Abstract
+{
+    public interface ISupportHyperMedia
+    {
+        List<HypermediaLink> Links { get; set; }
+    }
+}
